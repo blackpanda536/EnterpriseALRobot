396e8633de6499865e9c497719c1e6db93b9794c
blackpanda536 <66731563+blackpanda536@users.noreply.github.com>
2020-06-10 18:10:09 +0530

    welcome.py

--- tg_bot/modules/welcome.py
+++ tg_bot/modules/welcome.py
@@ -125,7 +125,7 @@ def new_member(bot: Bot, update: Update, job_queue: JobQueue):
 
             # Welcome yourself
             elif new_mem.id == bot.id:
-                update.effective_message.reply_text("Watashi ga kitta!")
+                update.effective_message.reply_text("Thanks for adding me.")
 
             else:
                 # If welcome message is media, send with appropriate function
