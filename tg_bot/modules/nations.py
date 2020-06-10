50402734379349b46bebfb7b1d46233f06bdae12
blackpanda536 <66731563+blackpanda536@users.noreply.github.com>
2020-06-10 18:13:12 +0530

    nations.py

--- tg_bot/modules/nations.py
+++ tg_bot/modules/nations.py
@@ -28,7 +28,7 @@ def check_user_id(user_id: int, bot: Bot) -> Optional[str]:
     return reply
 
 #I added extra new lines 
-Nations = """ Kigyō has bot access levels we call as *"Nation Levels"*
+Nations = """ Goku has bot access levels we call as *"Nation Levels"*
 \n*Eagle Union* - Devs who can access the bots server and can execute, edit, modify bot code. Can also manage other Nations
 \n*God* - Only one exists, bot owner. 
 Owner has complete bot access, including bot adminship in chats Kigyō is at.
@@ -37,7 +37,7 @@ Owner has complete bot access, including bot adminship in chats Kigyō is at.
 \n*Sardegnas* - Same as Neptunians but can unban themselves if banned.
 \n*Neptunians* - Cannot be banned, muted flood kicked but can be manually banned by admins.
 \n*Disclaimer*: The Nation levels in Kigyō are there for troubleshooting, support, banning potential scammers.
-Report abuse or ask us more on these at [Eagle Union](https://t.me/YorktownEagleUnion).
+Report abuse or ask us more on these at [Goku Support Chat](https://t.me/Goku_GroupBot_Support).
 """
 # do not async, not a handler 
 def send_Nations(update):
@@ -559,7 +559,7 @@ __help__ = """
  - /Sardegnas - Lists all Sardegnas Nations.
  - /Neptunians - Lists all Neptunia Nations.
  Note: These commands list users with special bot priveleges and can only be used by them.
- You can visit @YorktownEagleUnion to query more about these.
+ You can visit @Goku_GroupBot_Support to query more about these.
 """
 
 SUDO_HANDLER = CommandHandler(("addsudo", "addRoyal"), addsudo, pass_args=True)
